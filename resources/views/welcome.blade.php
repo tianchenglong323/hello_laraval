eb5ca525b40fa2f0aa660c847ce7077661432414
tianchenglong <673431353@qq.com>
2018-11-29 08:44:45 +0800

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+
+                height: 100vh;
+
+            }
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 66px;
+            }
+        </style>
+    </head>
+    <body>
+
+         <div class="container">
+             <div class="content">
+                  <div class="title"> Hello Laravel - by tianchenglong </div>
+             </div>
+         </div>
+    </body>
+</html>
